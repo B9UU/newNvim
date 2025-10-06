93e2f5d1279356326a058de403dcd95ffc3110d0
laqo <brahim.boussaa1998@gmail.com>
2025-10-06 17:17:31 +0100

    add json/yml formatters

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "yaml", "yml" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.expandtab = true
+	end,
+})
 return {}
--- lua/custom/plugins/linting.lua
+++ lua/custom/plugins/linting.lua
@@ -7,14 +7,14 @@ return {
 
 		-- if Eslint error configuration not found : change MasonInstall eslint@version or npm i -g eslint at a specific version
 		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
+			-- javascript = { "eslint_d" },
+			-- typescript = { "eslint_d" },
+			-- javascriptreact = { "eslint_d" },
+			-- typescriptreact = { "eslint_d" },
+			-- svelte = { "eslint_d" },
 			python = { "pylint" },
-			markdown = { 'markdownlint' },
-			json = { "jsonlint" },
+			markdown = { "markdownlint" },
+			-- json = { "jsonlint" },
 		}
 
 		eslint.args = {
@@ -28,7 +28,7 @@ return {
 			end,
 		}
 
-		local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
